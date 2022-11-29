81384f0a8e27d27671f417925e701881061940a0
GorINIch73 <ggorinich@gmail.com>
2022-11-29 14:28:46 +0300

    восстановление

--- formprofil.h
+++ formprofil.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef FORMPROFIL_H
 #define FORMPROFIL_H
 
@@ -47,53 +46,3 @@ private:
 };
 
 #endif // FORMPROFIL_H
-=======
-#ifndef FORMPROFIL_H
-#define FORMPROFIL_H
-
-#include <QWidget>
-#include <QSqlDatabase>
-#include <QSqlTableModel>
-//#include <QSqlRelationalTableModel>
-//#include <QSqlRelationalDelegate>
-
-namespace Ui {
-class FormProfil;
-}
-
-class FormProfil : public QWidget
-{
-    Q_OBJECT
-
-public:
-    explicit FormProfil(QSqlDatabase db,QWidget *parent = nullptr);
-    ~FormProfil();
-
-private slots:
-    void on_pushButton_First_clicked();
-
-    void on_pushButton_Prev_clicked();
-
-    void on_pushButton_Close_clicked();
-
-    void on_pushButton_Next_clicked();
-
-    void on_pushButton_Last_clicked();
-
-    void on_pushButton_Re_clicked();
-
-    void on_pushButton_Add_clicked();
-
-    void on_pushButton_Del_clicked();
-
-private:
-    Ui::FormProfil *ui;
-    QSqlDatabase base;
-    QSqlTableModel *model;
-    //QSqlRelationalDelegate *delegate;
-
-    void SetupTable();
-};
-
-#endif // FORMPROFIL_H
->>>>>>> 4eb8f4bbf917be0f1294fe3d8855518c914cbe29
--- qsqlrelationaldelegateflt.cpp
+++ qsqlrelationaldelegateflt.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "qsqlrelationaldelegateflt.h"
 
 #include <QCompleter>
@@ -60,66 +59,3 @@ QWidget *QSqlRelationalDelegateFlt::createEditor(QWidget *parent, const QStyleOp
 }
 
 
-=======
-#include "qsqlrelationaldelegateflt.h"
-
-#include <QCompleter>
-#include <QtDebug>
-#include <QSqlRecord>
-#include <QSqlField>
-#include <QLineEdit>
-
-
-QSqlRelationalDelegateFlt::QSqlRelationalDelegateFlt(QObject * parent) : QSqlRelationalDelegate(parent)
-{
-
-}
-
-QWidget *QSqlRelationalDelegateFlt::createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const
-{
-
-    // сделано специализировано для одной колонки
-    QString field = "answer"; //имя поля с ответом для комбобокса
-    int field_id = 4; // номер поля вопроса для фильта - имя не срабатывает так как поле дополнительное
-
-    // выковыриваем модель
-    const QSqlRelationalTableModel *sqlModel = qobject_cast<const QSqlRelationalTableModel *>(index.model());
-
-    // выковыриваем связанную модель
-    QSqlTableModel *childModel = sqlModel ? sqlModel->relationModel(index.column()) : nullptr;
-
-    // магия только для колонки с вопросами, остальным null
-    if (index.column()!=sqlModel->fieldIndex(field))
-        return nullptr;
-//        return QSqlRelationalDelegate::createEditor(parent, option, index);
-
-    // настраиваем комбобокс
-    QComboBox *combo = new QComboBox(parent);
-    combo->setModel(childModel);
-    combo->setModelColumn(childModel->fieldIndex(field));
-    // получить id вопроса
-    //qDebug() <<childModel->fieldIndex("answers_data.question_id");
-    QString qss= sqlModel->record(index.row()).value(field_id).toString();
-    //установить фильтер
-    childModel->setFilter(QString("question_id = \%1 ").arg(qss));
-
-    combo->setEditable(true);
-
-    // настраиваем комплитер
-    QCompleter *mycompletear = new QCompleter(parent);
-    mycompletear->setCaseSensitivity(Qt::CaseInsensitive);
-    //mycompletear->setFilterMode(Qt::MatchContains);
-    mycompletear->setCompletionMode(QCompleter::InlineCompletion);
-    mycompletear->setModel(childModel);
-    mycompletear->setCompletionColumn(childModel->fieldIndex(field)); // номер колонки с данными подстановки
-    combo->setCompleter(mycompletear);
-
-    // выделить текст
-//    combo->lineEdit()->selectAll();  не вышло!
-
-    return combo;
-
-}
-
-
->>>>>>> 4eb8f4bbf917be0f1294fe3d8855518c914cbe29
